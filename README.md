bd27e6edd6699d8584d7ad66544eca4cdcac108c
AhmedFathiHassan <ahmedfathi010223@gmail.com>
2024-11-06 00:09:44 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Learn-Frappe
+
+An App to Learn How to develop in ERPnext 
+
+#### License
+
+MIT
---
+++ learnfrappe/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Learn-Frappe",
+			"type": "module",
+			"label": _("Learn-Frappe")
+		}
+	]
